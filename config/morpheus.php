a12083491831d95015d7b7ed61de4aa2f041f967
zohreh daeian <zohreh.daeian@yahoo.com>
2021-02-15 13:47:50 +0330

    add config morpheus sender account

--- config/morpheus.php
+++ config/morpheus.php
@@ -3,6 +3,8 @@ return
     [
         'base_url' => env('MORPHEUS_BASE_URL'),
 
+        'token' => env('MORPHEUS_TOKEN') ,
 
-        'token' => env('MORPHEUS_TOKEN')
+        'sender_account' => env('MORPHEUS_SENDER_ACCOUNT'),
+
     ];
