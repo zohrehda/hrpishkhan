9023ab8746490d17b1a33e25a8c6cf9daf7f7295
zohreh <zohreh_daeian@yahoo.com>
2021-11-14 15:57:41 +0330

    edit2 card id

--- resources/views/requisitions/index.blade.php
+++ resources/views/requisitions/index.blade.php
@@ -6,13 +6,13 @@
                     <div class="col-md-4 form-space">
                         <div class="card {{ $card_class }}  text-white">
                             <div class="card-header text-center"
-                                 data-toggle="collapse" type="button"  data-target="#card-{{$requisition->id}}-{{$requisition->status}}">
+                                 data-toggle="collapse" type="button"  data-target="#card-{{$requisition->id}}-{{$card_class}}">
                                 {{ $requisition->en_title }}
                                 <br>
                                 {{$requisition->owner->details()['name']}}
                             </div>
 
-                            <div class="card-body collapse multi-collapse"  id="card-{{$requisition->id}}-{{$requisition->status}}" >
+                            <div class="card-body collapse multi-collapse"  id="card-{{$requisition->id}}-{{$card_class}}" >
                                 @foreach($requisition->progresses as $progress)
                                     <p class="card-text">
                                         {{ $progress->role }} - {{ $progress->user->name }} - {{ $progress->status }}
