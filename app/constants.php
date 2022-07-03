ba303465a44bb40da136dca3d83863d888a873f1
zohreh <zohreh_daeian@yahoo.com>
2022-07-03 10:41:39 +0430

    add autoload constants file

---
+++ app/constants.php
@@ -0,0 +1,11 @@
+<?php
+
+const FINAL_ACCEPT_ACTION = 'final_accept';
+const ASSIGN_ACTION = 'assign';
+
+const ACCEPTED_STATUS = 'accepted';
+const ASSIGNED_STATUS = 'assigned';
+const PENDING_STATUS = 'pending';
+
+const ADMIN_FINAL_PENDING = 'admin_final_pending';
+const ADMIN_PRIMARY_PENDING_PRG_STATUS = 'admin_primary_pending';
