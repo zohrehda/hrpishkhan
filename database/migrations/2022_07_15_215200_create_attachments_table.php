22e336e6fe06d5df0010ab48af1727359fa7cc73
zohreh <zohreh_daeian@yahoo.com>
2022-07-15 21:54:29 +0430

    attachment migration

---
+++ database/migrations/2022_07_15_215200_create_attachments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttachmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attachments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('requisition_id') ;
+            $table->foreign('requisition_id')->references('id')->on('requisitions')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->timestamps();
+            $table->string('name') ;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attachments');
+    }
+}
